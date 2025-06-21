ce30ac815ac7b88dc4034ee5e689efc5b55e729d
Ganesh <ganeshkanakamedala@gmail.com>
2025-06-21 07:32:56 +0530

    Examples and explanations for assignment operators in Python.

---
+++ Day_5_ASSIGNMENT_OPERATORS/Assignment_operators.py
@@ -0,0 +1,23 @@
+'''Assignment operators:  The assignment operators are used to assign a value to a variable. They are used to update th value of a variable.
+The assignment operators are as follows:-
+1. = (Assignment operator)
+2. += (Addition and assignment operator)
+3. -= (Subtraction and assignment operator)
+4. *= (Multiplication and assignment operator)
+5. /= (Division and assignment operator)
+6. %= (Modulus and assignment operator)
+7. **= (Exponentiation and assignment operator)
+8. <<= (Left shift and assignment operator)
+9. >>= (Right shift and assignment operator)
+10. &= (Bitwise AND and assignment operator)
+11. ^= (Bitwise XOR and assignment operator)
+12. |= (Bitwise OR and assignment operator)
+  '''
+# Examples
+x = 5
+x += 3  # x is now 8
+x -= 2  # x is now 6
+x *= 5  # x is now 30
+
+
+
