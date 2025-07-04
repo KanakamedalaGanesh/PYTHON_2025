cfef24c2af357ce39b4ab9101acd5bd36c654679
Ganesh <ganeshkanakamedala@gmail.com>
2025-07-04 12:29:28 +0530

    Explanations for identifiers and keywords.

--- Day_4_OPERATORS_and_EXPRESSIONS/Expressions.py
+++ Day_4_OPERATORS_and_EXPRESSIONS/Expressions.py
@@ -35,3 +35,12 @@ print(area)
 # c=1
 # q=(-b+math.sqrt(b**2-(4*a*c)))/2*a
 # print(q)
+
+
+# Identifier : An identifier is a name given to a variable, function, class, module etc. in a program. It is used to access the variable, function, class, module etc. in a program. 
+# Identifiers can be of any length but they cannot start with a digit. They can contain letters , digits, and underscores. They are case sensitive. For example, a and A are two different identifiers. 
+# Example: x,y,z,my_variable,my_function,my_class,my_module etc. 
+
+# Keywords : Keywords are the reserved words in a programming language. They have a special meaning in the language and cannot be used as identifiers. They are used to define the syntax of the language. 
+# Example: if,else,for,while,def,class,import,pass etc.
+
